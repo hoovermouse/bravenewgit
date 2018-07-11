aa757268e04d175906a70a609e664f8106cb9001
Berna.Balci@ogd.nl <Berna.Balci@ogd.nl>
2018-07-11 13:17:01 +0200

    added commnt

--- halpwutizsplat.ps1
+++ halpwutizsplat.ps1
@@ -11,6 +11,13 @@ param(
 
 Function NewUsername ($firstname, $surname, $user, $newusername, $prepositions)
 {
+    <#Het moet het volgende kunnen doen
+    get-aduser BEBA00 | Rename-ADObject -NewName "be ba"
+set-aduser BEBA00 -surname ba -displayname "ba, be" -UserPrincipalName b.ba@domain.com -EmailAddress b.ba@domain.com
+
+#in exchange shell 
+Set-Mailbox b.ba -PrimarySmtpAddress b.ba@domain.com -EmailAddressPolicyEnabled $false 
+#>
 
 Rename-ADObject $user -NewName $newusername
 set-adobject $newname -surname $surname.Trim() -displayname $Displayname
